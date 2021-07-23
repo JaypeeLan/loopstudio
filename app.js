bcb0714a151ae24c0a55cbac4a9044d265b32fb3
JaypeeLan <jplaniran01@gmail.com>
2021-07-23 08:36:32 +0100

    fixed navbar

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bg = document.getElementById('nav-page');
 const menuItems = document.querySelector('.menu_links');
 
 menu.addEventListener('click', function(){
-    // menu.classList.toggle('is-active'),
+    menu.classList.toggle('is-active'),
     bg.classList.toggle('active'),
     menuItems.classList.toggle('active')
 });
